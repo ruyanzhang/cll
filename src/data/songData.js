daa1bafa5861108e933fca5fc0bda8ef4c1fde33
ruyanzhang <ru1zhang@21cn.com>
2020-03-14 00:07:20 +0800

    new

--- src/data/songData.js
+++ src/data/songData.js
@@ -5,7 +5,7 @@ export default {
             "older":"0",
             "musicName": "爸爸去哪儿",
             "musicSinger": "林志颖",
-            "musicLink": "http://fs.w.kugou.com/201804212318/0a7999a9bd957a1bfb029d81f32402af/G129/M04/1A/0D/wQ0DAFo7ZWSATZUQAEMnMZ9qtxQ518.mp3",
+            "musicLink": "https://isure.stream.qqmusic.qq.com/C400001HOipj44jNRk.m4a?guid=731064280&vkey=8C156740090CF9B29543D2E86A337FECD8FA614C7678B5DC3E0FBDC48893EE821D1F53A830A6718751074356211F79D5450B2ED9966C198C&uin=0&fromtag=66",
             "lyricsLink": "src/lrc/bbqne.lrc",
             "albumCover": "src/images/albumCover/bbqne.jpg"
         },
@@ -13,7 +13,7 @@ export default {
             "older":"1",
             "musicName": "愿得一人心",
             "musicSinger": "李行亮",
-            "musicLink": "http://fs.w.kugou.com/201804221245/0df5eb3c35caa9662fad887c8b7d7047/G006/M05/0F/13/poYBAFUGIICAWM0cAEOl-W9aLT4123.mp3",
+            "musicLink": "https://isure.stream.qqmusic.qq.com/C400004Zouvt3tHgUs.m4a?guid=731064280&vkey=843080B6D3BC06E70A8054F01E64C0531F12E2D4AFD23559B79E1E96B379AB764A79039E60C37532299A3A2DDA900DBF81AEBB6EA0C41D49&uin=4394&fromtag=66",
             "lyricsLink": "src/lrc/ydyrx.lrc",
             "albumCover": "src/images/albumCover/ydyrx.jpg"
         },
@@ -21,7 +21,7 @@ export default {
             "older":"2",
             "musicName": "小酒窝",
             "musicSinger": "林俊杰 蔡卓妍",
-            "musicLink": "http://fs.w.kugou.com/201804221701/9cbb8f85a24628efcb9eb52869f92da9/G004/M02/08/19/RA0DAFT7ydWAH0BVADUzjebdjF4562.mp3",
+            "musicLink": "https://isure.stream.qqmusic.qq.com/C400002v0o782hcgib.m4a?guid=731064280&vkey=80D74BE2779FC4B8A32C3E4A8B9BB7883CD7E3499636334AE4D1826084D1AB10D1248335B717D87E5314A2029567A115A44EF8917D65CCAD&uin=4394&fromtag=66",
             "lyricsLink": "src/lrc/xjw.lrc",
             "albumCover": "src/images/albumCover/xjw.jpg"
         }
